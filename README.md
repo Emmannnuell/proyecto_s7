4a21fe8568fe1ac892f7013c1553eee774f345c6
Emmanuel <emm_azul21@hotmail.com>
2025-08-09 17:19:51 -0600

    Proyecto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# proyecto_s7 building
+# proyecto_s7 
+
+Esta aplicación interactiva permite analizar un conjunto de datos de anuncios de venta de vehículos. Los datos se encuentran dentro del dataset vehicles_us. Se pueden visualizar diferentes aspectos de los datos utilizando un histograma y un gráfico de dispersión, y se incluyen botones interactivos que permiten la visualización de cada gráfico. El script de python se encuentra comoen el archivo app.py.
+
+Alumno: Emmanuel Romero DA50
--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-     
+
+st.header("Dashboard de Análisis de Vehículos")
+st.subheader("Proyecto Sprint 7")
+
+
 car_data = pd.read_csv('datasets/vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir un gráfica de dispersión') # crear un botón
      
 if hist_button: # al hacer clic en el botón
-     # escribir un mensaje
+     # mensaje
      st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         
      # crear un histograma
      fig = px.histogram(car_data, x="odometer")
-     
      # mostrar un gráfico Plotly interactivo
      st.plotly_chart(fig, use_container_width=True)
+
+
+if disp_button:
+    # mensaje
+    st.write('Creacion de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+    #Creación de gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
      
